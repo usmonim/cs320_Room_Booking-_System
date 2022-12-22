799e9b259ae7b19e7db682ece26eb0fd0d9b3ac7
dogaakin9 <115349598+dogaakin9@users.noreply.github.com>
2022-12-22 19:43:50 +0300

    Update RoomActions.java
    creation of constructor

--- room_booking/src/room_booking_system/RoomActions.java
+++ room_booking/src/room_booking_system/RoomActions.java
@@ -23,4 +23,16 @@ public class RoomActions {
 		this.conn = conn;
 		this.stmt = stmt;
 	}
+	
+	
+	
+	//Creation Constructor 
+	public RoomActions(String room_name, int building_id, int capacity, String property_string) {
+		this.room_name = room_name;
+		this.building_id = building_id;
+		this.capacity = capacity;
+		this.property_string = property_string;
+//		this.conn = conn;
+		// TODO Auto-generated constructor stub
+	}
   }
