103c83bc1e620cefc5bc6c98e792b4d56d77749b
NazifBoz <nazif.boz@ozu.edu.tr>
2022-12-23 21:47:54 +0300

    Creating the login page class.

---
+++ room_booking/src/room_booking_system/LoginPage.java
@@ -0,0 +1,21 @@
+package assignment1RoomReservation;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginPage implements ActionListener {
+
+    private static String adminName = "admin123";
+
+    private static JLabel label;
+    private static JTextField admText;
+    private static JButton adm_button;
+    private static JButton usr_button;
+    public static JFrame frame;
+
