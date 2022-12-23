a7ea32dd3ae376af57449b016151fd0f6649495f
Muhamad Usmoni <usmoni.muhamad_main2001@protonmail.com>
2022-12-23 09:50:32 +0200

    List rooms in building function implementation

--- room_booking/src/room_booking_system/BuildingActions.java
+++ room_booking/src/room_booking_system/BuildingActions.java
@@ -52,6 +52,29 @@ public class BuildingActions {
 		
 	}
 	
+	// List rooms in building 
+	public void listRooms() throws Exception { 
+		
+		String query = "select room.id, room_name, building_name, capacity, reservation_status from room, building where building.id = " + building_id + " and room.building_id = " + building_id + ";";
+		
+		ResultSet rs =  stmt.executeQuery(query);
+		 while (rs.next()) {
+			 int room_id  = rs.getInt("id");
+	         String room_name = rs.getString("room_name");
+	         String building_name = rs.getString("building_name");
+	         int capacity = rs.getInt("capacity");
+	         String reservation_status = rs.getString("reservation_status");
+
+	         System.out.print("Room ID: " + room_id);
+	         System.out.print("|  Room Name: " + room_name);
+	         System.out.print("|  Building Name: " + building_name);
+	         System.out.print("|  Capacity: " + capacity);
+	         System.out.println("|  Reservation Status: " + reservation_status);
+	      }   
+		
+		
+	}
+	
 	
 	
 	
