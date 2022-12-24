b2ebe40468c7c3fdb99a3c5e21c5326fcd392d80
ruzgargumus <101419320+ruzgargumus@users.noreply.github.com>
2022-12-24 18:17:40 +0300

    Create UserActions.java

---
+++ room_booking/src/room_booking_system/UserActions.java
@@ -0,0 +1,46 @@
+package assignment1RoomReservation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserActions {
+	
+	Connection conn;
+	Statement stmt;
+	
+	int user_id;
+
+
+	public UserActions() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserActions(int user_id_id) {
+		this.user_id = user_id_id;
+	
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+	//Connections
+	public void connections(Connection conn, Statement stmt ) {
+			this.conn = conn;
+			this.stmt = stmt;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("..............MMMMMMM..............");
+
+	}
+	
+	
+
+	}
+
+}
