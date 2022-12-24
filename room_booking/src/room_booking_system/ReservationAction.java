5209bddf19dc2c4a87d558f9ea4291e3971c0fec
dogaagdas <80897650+dogaagdas@users.noreply.github.com>
2022-12-24 20:20:20 +0300

    Update ReservationAction.java
    Function roomreserv() created. Also ResultSet interface represents the set of results of a Select statement and used to process the rows returned by a Select statement and retrieve the values from the columns of those rows.

--- room_booking/src/room_booking_system/ReservationAction.java
+++ room_booking/src/room_booking_system/ReservationAction.java
@@ -52,5 +52,14 @@ public class ReservationActions {
 		}
 	}
 	
+	public void roomreserv() throws Exception {
+		
+		String get_status = "select reservation_status from room where id = " + room_id + ";";
+		String get_roomNameForcheck = "select room_name from room where id = " + room_id + ";";
+		ResultSet rs =  stmt.executeQuery(get_status);
+		ResultSet rsRoom_name =  stmt.executeQuery(get_roomNameForcheck);
+		
+	}
+	
 	
 }
