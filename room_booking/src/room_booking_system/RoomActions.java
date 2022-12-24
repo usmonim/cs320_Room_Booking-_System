ba333100a238d24755b41e0bea6d5244389c5733
Muhamad Usmoni <usmoni.muhamad_main2001@protonmail.com>
2022-12-24 17:38:36 +0200

    Coments and code explanation

--- room_booking/src/room_booking_system/RoomActions.java
+++ room_booking/src/room_booking_system/RoomActions.java
@@ -1,4 +1,4 @@
-package assignment1RoomReservation;
+package room_booking_system;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -43,7 +43,8 @@ public class RoomActions {
 //		this.stmt = stmt;
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	// functions for creating a new room in db
 	public void creation() throws Exception {
 
 		PreparedStatement preparedStatement = conn.prepareStatement("insert into room (room_name, building_id, capacity, property_string, reservation_status) values(?, ?, ?, ?, ?)");
@@ -56,7 +57,7 @@ public class RoomActions {
 		System.out.println("Room " + room_name + " has been created succesfully.");
 
 	}
-
+	// functions for deleating a room in db but first it checks if this room is reserved or not. 
 	public void deletion() throws Exception {
 
 		String get_reserveIfexist = "select* from reserves where room_id = " + room_id + ";";
@@ -81,7 +82,7 @@ public class RoomActions {
 		}
 	}
 
-
+	// functions fro getting all occupied/reseved rooms from DB 
 	public void getoccupied() throws Exception {
 
 
@@ -103,7 +104,8 @@ public class RoomActions {
 		}
 
 	}
-
+	
+	//Searching a room with speciified sstring. If we want a room with laptop we give an input and it searches in rooms properties if there exist a room with laptop
 	public void search(String template) throws Exception {
 		String get_satusfyingRooms = "select id, room_name, building_id, capacity, reservation_status, property_string from room where property_string like '%" + template + "%';";
 		ResultSet rs = stmt.executeQuery(get_satusfyingRooms);
