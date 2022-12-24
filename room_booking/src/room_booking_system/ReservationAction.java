8858c440e309513bf9388c684976659e664fdeb7
dogaagdas <80897650+dogaagdas@users.noreply.github.com>
2022-12-24 19:33:04 +0300

    Update ReservationAction.java
    Created the class ReservationActions.java and connected java with sql database with import.

--- room_booking/src/room_booking_system/ReservationAction.java
+++ room_booking/src/room_booking_system/ReservationAction.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 package room_booking_system;
->>>>>>> 10613b07ffa4b6e15a1b1165ba1e906113d2a058
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,58 +7,7 @@ import java.sql.Statement;
 
 public class ReservationActions {
 
-    int room_id;
-
-    int user_id;
-
-
-    Connection conn;
-    Statement stmt;
-
-<<<<<<< HEAD
-    public ReservationActions() { //Empty Constructer
-        // TODO Auto-generated constructor stub
-    }
-
-    public ReservationActions(int room_id) {
-        this.room_id = room_id;
-        // TODO Auto-generated constructor stub
-    }
-
-    public ReservationActions(int room_id, int user_id) {
-        this.room_id = room_id;
-        this.user_id = user_id;
-    }
-
-    //Connections
-    public void connections(Connection conn, Statement stmt ) {
-        this.conn = conn;
-        this.stmt = stmt;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("..............MMMMMMM..............");
-
-    }
-
-
-=======
-    public ReservationActions() {
-		// TODO Auto-generated constructor stub
-	}
 	
-	public ReservationActions(int room_id) {
-		this.room_id = room_id;
-		// TODO Auto-generated constructor stub
-	}
 	
-	public ReservationActions(int room_id, int user_id) {
-		this.room_id = room_id;
-		this.user_id = user_id;
-	}
-
-	//Connections
-	public void connections(Connection conn, Statement stmt ) {
-			this.conn = conn;
-			this.stmt = stmt;
-	}
+	
+}
