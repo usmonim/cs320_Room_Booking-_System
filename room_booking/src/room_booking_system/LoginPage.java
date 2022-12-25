f858288dfead4ccf896665e3ed6c5b130513e274
dogaakin9 <115349598+dogaakin9@users.noreply.github.com>
2022-12-25 20:18:54 +0300

    Update LoginPage.java
    login page recreated, panel and frame are created as well

--- room_booking/src/room_booking_system/LoginPage.java
+++ room_booking/src/room_booking_system/LoginPage.java
@@ -1,1 +1,29 @@
+package assignment1RoomReservation;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginPage implements ActionListener {
+  
+  private static JLabel label;
+	private static JTextField admText;
+	private static JButton adm_button;
+	private static JButton usr_button;
+	public static JFrame frame;
+  
+  public static void main(String[] args) {
+    JPanel panel = new JPanel();
+		
+
+		frame = new JFrame();
+		frame.setSize(500, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+  }
+}
